e9d9d99c0acb6393146a2d8ba7297de23395e21b
Shuaib Kaldane <shuaib.kaldane@vit.edu.in>
2026-04-15 13:08:48 +0530

    Random Bill Pay

---
+++ Bill.py
@@ -0,0 +1,4 @@
+import random
+name = ["Shuaib" , "Abdullah" , "Sankeet"]
+choice = random.randint(0,2);
+print(name[choice])
---
+++ Head_Tail.py
@@ -0,0 +1,8 @@
+# Program for Tails and Head
+import random 
+coin = random.randint(1 ,2 )
+if(coin == 1):
+    print("Head")
+elif(coin == 2):
+    print("Tails")
+
