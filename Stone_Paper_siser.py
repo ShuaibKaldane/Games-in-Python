7363a20081dba94344321d42e13f9d5ed0769cd2
Shuaib Kaldane <shuaib.kaldane@vit.edu.in>
2026-04-15 13:51:57 +0530

    optimized code

--- Stone_Paper_siser.py
+++ Stone_Paper_siser.py
@@ -6,7 +6,11 @@ rules = ["Rock" , "Paper" , "Sisers"]
 name = input("Enter your name\n")
 computer_choice = random.randint(0,2)
 user = int (input("Enter 0 for Rock , 1 for paper , 2 for Sisers\n"))
-if(computer_choice == user):
+if(user not in [0,1,2]):
+    print("Invalid input")
+elif(computer_choice == user):
+    print(f"Computer Choice : {rules[computer_choice]}")
+    print(f"User Choice : {rules[user]}")
     print("Draw")
 elif(computer_choice == 0 and user == 1):
     print(f"Computer Choice : {rules[computer_choice]}")
