61048b32c484dc4352bf6952a2f2c2a61e9e71f9
Shuaib Kaldane <shuaib.kaldane@vit.edu.in>
2026-05-09 12:28:57 +0530

    Changes in the Auction program

--- Auction_Program.py
+++ Auction_Program.py
@@ -3,17 +3,16 @@ def Auction():
     highest_bid = 0
     winner = ""
     while True:
-        name = input("Enter the bidder name\n")
-        value = int(input("Enter the ammount $ \n"))
-        data[name] = value
-        another = input("Is there any another bidder Yes or no\n").lower()
-        if (another == "no"):
+        name = input("Enter the bidder name \n")
+        ammount = int(input("Enter the ammount $ \n"))
+        data[name]= ammount
+        other = input("IS there any other bidder yes or no\n").lower()
+        if(other == "no"):
             break
-    print(f'The biders are {data}')
-    for element in data :
-        if (data[element] >highest_bid):
-            highest_bid = data[element]
-            winner = element
-    print(f"The highest bidder name is {winner} and the ammount is $ {highest_bid}")
-
+    print(f'Available Bidder are : {data}')
+    for element in data:
+     if(data[element] > highest_bid):
+                highest_bid = data[element]
+                winner = element
+    print(f'Winner of this Bid is {winner} and the ammount is $ {highest_bid}')
 Auction()
